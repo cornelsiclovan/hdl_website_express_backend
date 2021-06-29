c818ace25c566cbbd8fa4ba00981d7a3f3366038
cornelsiclovan <cornel.siclovan@gmail.com>
2021-06-29 13:44:35 +0300

    bug fixes

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,7 @@ const config = require('config');
 
 module.exports = function() {
     const db = config.get('db');
+    
     mongoose.connect(config.get('db'))
         .then(() => winston.info(`Connected to ${db}...`))
 }
