987f2bdb0dc32487dcd1d05e67a91ebf70bf3d85
cornelsiclovan <cornel.siclovan@gmail.com>
2021-07-14 09:12:23 +0300

    .env

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,12 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = function() {
     const db = config.get('db');
     
-    mongoose.connect(config.get('db'))
-        .then(() => winston.info(`Connected to ${db}...`))
+    mongoose.connect(`mongodb://${process.env.MONGODB_HOST}/hdl_db`).
+        then(() => winston.info(`Connected to ${db}...`))
 }
